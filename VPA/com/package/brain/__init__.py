9330537e4cb0ba3631044251c31bd6adf12a3242
bhayana <divyabhayana7@yahoo.co.in>
2016-09-16 15:50:55 +0200

    Added logic for what you speak

--- VPA/com/package/brain/__init__.py
+++ VPA/com/package/brain/__init__.py
@@ -5,6 +5,7 @@ from time import ctime
 import wikipedia
 import urllib3
 import logging
+from logging.config import thread
 
 speech_engine = pyttsx.init('sapi5') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
 speech_engine.setProperty('rate', 150)
@@ -19,13 +20,8 @@ def listen():
     with speech_recognition.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
         print("Set minimum energy threshold to {}".format(recognizer.energy_threshold))
-        #=======================================================================
-        # a = recognizer.record(source, duration=4, offset=False)
-        #=======================================================================
+       
         audio = recognizer.listen(source)
-        #=======================================================================
-        # print(a)
-        #=======================================================================
         print(audio)
     try:
         return recognizer.recognize_google(audio)
@@ -37,8 +33,7 @@ def listen():
         return "Could not understand audio"
     return ""
 
-#speak("Say something!")
-#speak("I heard you say " + listen())
+
 def jarvis(data):
     if "wake up" in data:
         speak("yes")
@@ -47,23 +42,49 @@ def jarvis(data):
     if "what time is it" in data:
         speak(ctime()) 
 
+def logic():
+    speak("Hi Divya, what can I do for you?")
+    #listen again if sound is not clear#
+    loop=True
+    while loop:
+        data=listen()
+        if "Could not understand audio" in data:
+            speak("please speak again")
+            continue
+        else:
+            loop=False
+    
+    print (data)
+    if "news" in data:
+        print("call java to parse site https://news.google.co.in/")
+    elif "weather" in data:
+        print("call java to parse site https://weather.co.in/")
+    else:
+        print (wikipedia.search(data, results=3, suggestion=False))
+        speak("which one")
+        data=listen()
+        speak(wikipedia.summary(data, sentences=2, chars=0, auto_suggest=True, redirect=True))
+    
+    speak("You want to know anything else")
+    data=listen()
+    if "no" in data:
+        speak("bye bye")
+    else:
+        logic()
+    return "done";
 
 time.sleep(2)
-speak("Hi Divya, what can I do for you?")
-data=listen()
-print (data)
-if "Could not understand audio" in data:
-    speak("please speak again")
-    data=listen()
-print (wikipedia.search(data, results=3, suggestion=False))
-speak("which one")
-data=listen()
-speak(wikipedia.summary(data, sentences=2, chars=0, auto_suggest=True, redirect=True))
+count=1
+while 1:
+    print(thread.__name__)
+    print(count)
+    data = listen()
+    if "Jarvis" in data:
+        result = logic();
+        
+        
 
-#===============================================================================
-# while 1:
-#     speak("my job")
-#     data = listen()
-#     speak("ok")
-#     jarvis(data)
-#===============================================================================
+    
+    
+
+
